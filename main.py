99926f6e0664a27778cd3a88778a724efcbe84c0
malaikanaz <malaikanaz280@gmail.com>
2024-11-11 22:22:48 -0800

    Update main.py

--- main.py
+++ main.py
@@ -368,3 +368,31 @@ def val_ue(n):
         print("EVEN")
 val_ue(n)
 # -----------------------------------------------------------------------------------------------------------------
+# question no 45
+# factorial of n using recursion
+n=int(input("enter a number: "))
+def fac(n):
+    if (n==0 or n==1):
+        return 1
+    return fac(n-1)* n
+print(fac(n))
+# -------------------------------------------------------------------------------------------------------------------
+# question no 46
+# write a recursive function to calculate the sum of first n natural numbers
+n=int(input("enter a natural num:  "))
+def calc_sum(n):
+    if (n==0):
+        return 0
+    return calc_sum(n-1) + n
+print(calc_sum(n))
+# --------------------------------------------------------------------------------------------------------------------
+# question no 47
+# write a recursive function to print all the elements in a list.[hint: use list and index as parameters]
+def print_list(list,idx=0):
+    if idx == len(list):
+        return
+    print(list[idx])
+    print_list(list, idx+1)
+cities=['islamabad','lahore','faisalabad']
+print_list(cities)
+# ------------------------------------------------------------------------------------------------------------------
